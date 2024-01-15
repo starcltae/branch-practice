4d41dd5dd710f6e9ea84cdc30dbe2d9eb2a5436f
gyoungtae-min <p.amin2719@gmail.com>
2024-01-15 12:08:29 +0900

    Trailing Comma Practice

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import randint
+ifrom random import randint
 from random import choice
 
 def get_randint() -> list:
@@ -7,6 +7,19 @@ def get_randint() -> list:
 
 def get_choice() -> list:
 	return [choice(range(1,45+1)) for _ in range(7) ]
+
+
+fruits = [
+			'apple', 
+			'banana'
+]
+
+foods = [
+			'pasta',
+		    'pizza'
+]
+
+
 
 
 if __name__=='__main__':
